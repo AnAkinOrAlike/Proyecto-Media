0610c6c6e344662ae40549440b125d664eaae920
AnAkinOrAlike <andresacm166@gmail.com>
2024-06-24 04:51:28 -0400

    ID modificados para la funcion de color elegido

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('colorPicker').addEventListener('change', function() {
         console.log(this.value);
         var selectedColor = this.value;
-        document.getElementById('hr').style.backgroundColor = selectedColor;
-        document.getElementById('mhr').style.backgroundColor = selectedColor;
+        document.getElementById('headline').style.backgroundColor = selectedColor;
+        document.getElementById('box').style.backgroundColor = selectedColor;
         
         for (let i = 1; i <= 5; i++) {    
             let starId = 'star' + i;
-            let element = document.getElementById(starId);
+            let element = document.getElementById(bxstar);
             if (element) {
-                element.style.setProperty('--bg-color', selectedColor);
+                element.style.setProperty('--hex-color', selectedColor);
 }}})});
 
 document.addEventListener('DOMContentLoaded', function() {
